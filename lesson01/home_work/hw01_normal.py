dbf484ed6836ea91957d66625105ae27b6b95af2
Denis Sorokin <denys-s@yit.co.il>
2017-02-01 16:48:45 +0200

    Added config

--- lesson01/home_work/hw01_normal.py
+++ lesson01/home_work/hw01_normal.py
@@ -1,43 +1,43 @@
+import math
 # Задача-1: Дано произвольное целое число, вывести самую большую цифру
 # этого числа
-number = 123865748021
-result = 0
-while number:
-    number = number // 10
-    result = number % 10 if result < number % 10 else result
-print(result)
+NUMBER = 123865748021
+RESULT = 0
+while NUMBER:
+    NUMBER = NUMBER // 10
+    RESULT = NUMBER % 10 if RESULT < NUMBER % 10 else RESULT
+print(RESULT)
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
 # Поменять значения переменных местами. Вывести новые значения на экран.
 # Решите задачу используя только две переменные
-numberA = int(input("numberA = "))
-numberB = int(input("numberB = "))
-numberA = numberA + numberB
-numberB = numberB - numberA
-numberB = -numberB
-numberA = numberA - numberB
-print("numberA = ", numberA, "numberB = ", numberB)
+NUMBER_A = float(input("numberA = "))
+NUMBER_B = float(input("numberB = "))
+NUMBER_A = NUMBER_A + NUMBER_B
+NUMBER_B = NUMBER_B - NUMBER_A
+NUMBER_B = -NUMBER_B
+NUMBER_A = NUMBER_A - NUMBER_B
+print("a = ", NUMBER_A, "b = ", NUMBER_B)
 
 # Задача-3: Напишите программу, вычисляющую корни квадратного уравнения вида ax2 + bx + c = 0.
 # Для вычисления квадратного корня воспользуйтесь функицй sqrt() молудя math
 # import math
 # math.sqrt(4) - вычисляет корень числа 4
-import math
-print(" *** Hi, I am a program that can help you with \"quadratic equation\" like (ax2 + bx + c = 0)")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
-d = b**2 - 4 * a * c
-x1 = 0
-x2 = 0
-print("Discriminant = ", d)
-if d > 0:
-    x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / 2 * a
-    x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / 2 * a
-    print("x1 = ", round(x1, 4), " x2 = ", round(x2, 4))
-elif d == 0:
-    x1 = -b / 2 * a
-    x2 = x1
-    print("x1 = ", round(x1, 4), " x2 = ", round(x2, 4))
+print("** Hi, I am a program that can help you with \"quadratic equation\" like (ax2 + bx + c = 0)")
+A = float(input("a = "))
+B = float(input("b = "))
+C = float(input("c = "))
+D = B**2 - 4 * A * C
+X1 = 0
+X2 = 0
+print("Discriminant = ", D)
+if D > 0:
+    X1 = (-B + math.sqrt(B**2 - 4 * A * C)) / 2 * A
+    X2 = (-B - math.sqrt(B**2 - 4 * A * C)) / 2 * A
+    print("x1 = ", round(X1, 4), " x2 = ", round(X2, 4))
+elif D == 0:
+    X1 = -B / 2 * A
+    X2 = X1
+    print("x1 = ", round(X1, 4), " x2 = ", round(X2, 4))
 else:
     print("Roots not exists on the set of real numbers")
