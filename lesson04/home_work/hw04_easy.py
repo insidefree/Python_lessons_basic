4c839b2f5e9a51bc7f1a424c804e4aa1f5a26161
Denis Sorokin <denys-s@yit.co.il>
2017-02-16 14:41:34 +0200

    Added hw

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -1,3 +1,5 @@
+from random import randint
+
 # Все задачи текущего блока решите с помощью генераторов!
 
 # Задание-1:
@@ -5,11 +7,25 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+random_list = [randint(-25, 25) for _ in range(10)]
+pow_list = [el ** 2 for el in random_list]
+
+print(random_list)
+print(pow_list)
+
 # Задание-2:
 # Даны два списка фруктов. Получить список фруктов, присутствующих в обоих исходных списках.
+fruit_list_01 = ['apple', 'watermelon', 'lemon', 'cherry']
+fruit_list_02 = ['pear', 'apple', 'cherry']
+
+result_list = [el_01 for el_01 in fruit_list_01 if el_01 in fruit_list_02]
+print("Result that contains elements from several lists is {}".format(result_list))
 
 # Задание-3:
 # Дан список, заполненный произвольными числами. Получить список из элементов исходного, удовлетворяющих след. условиям:
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+result_list = [el for el in random_list if el % 3 == 0 and el >= 0 and el % 4 != 0]
+print("Result list is {}".format(result_list))
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,7 @@
+import re
+import os
+from random import randint
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые окружают 1 или более символа в верхнем регистре.
 # Решить задачу двумя способами: с помощью re и без.
@@ -14,23 +18,61 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIP
        'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
        'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+print(re.findall(r'[A-Z]{3,}', line))
+temp_list = [el[0].lower() + el[-1].lower() for el in re.findall(r'[A-Z]{3,}', line)]
+print(temp_list)
+
 # Задание-2:
 # Вывести символы в верхнем регистре, которые окружают ровно два символа в нижнем регистре слева
 # и два символа в верхнем регистре справа. Решить задачу двумя способами: с помощью re и без.
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
-       'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
-       'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
-       'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
-       'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
-       'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
-       'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
-       'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
-       'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
-       'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
-       'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
-       'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+         'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
+         'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
+         'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
+         'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
+         'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
+         'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
+         'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
+         'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
+         'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
+         'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
+         'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+temp_list = [el[0].capitalize() + el[-1].capitalize() for el in
+             re.findall(r'[A-Za-z]{1,1}[a-z]{2,2}[A-Z]{2,2}[A-Za-z]{1,1}', line)]
+print(temp_list)
 
 # Задача-3:
 # Напишите скрипт заполняющий указанный файл (самомстоятельно задайте имя файла) произвольными целыми
 # цифрами, в результате в файле должно быть 2500-значное произвольное число
 # Найдите и выведите самую длинную последовательность одинаковых цифр в вышезаполненном файле
+
+file_name = input("Insert file name: ")
+DIR = 'files'
+
+
+def number_by_category_generator():
+    category = input("Enter category of number: ")
+    category = int(category)
+    number = 0
+    i = 0
+    while i < category:
+        number += randint(0, 9) * 10 ** i
+        i += 1
+    return number
+
+
+def fill_file_random_num(file_name):
+    path = os.path.join(DIR, file_name + ".txt")
+    if not os.path.exists(DIR):
+        os.makedirs(DIR)
+    try:
+        with open(path, 'w', encoding="UTF-8") as f:
+            f.write(str(number_by_category_generator()))
+    except ValueError:
+        print("Error happened: {}".format(ValueError))
+
+
+fill_file_random_num(file_name)
+number = open(DIR + "/n.txt")
+print(re.findall(r'', number))
