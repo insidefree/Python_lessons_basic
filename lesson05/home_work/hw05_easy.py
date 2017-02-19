87730b1b47d4a1f51498ca97553e76ee3491ba9d
Denis Sorokin <denys-s@yit.co.il>
2017-02-19 15:05:47 +0200

    hw05

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,11 +1,48 @@
+import os
+import shutil
+
 # Задача-1:
 # Напишите скрипт создающий директории dir_1 - dir_9 в папке из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+
+def create_folders(folder_name):
+    try:
+        os.mkdir(folder_name)
+        print("Folder {} was created".format(folder_name))
+    except FileExistsError as e:
+        if e.errno == 17:
+            print("Directory {} is exist".format(folder_name))
+
+
+def remove_folders(folder_name):
+    try:
+        os.rmdir(folder_name)
+        print("Folder {} was deleted".format(folder_name))
+    except FileNotFoundError as e:
+        if e.errno == 2:
+            print("Directory \"dir_{}\" doesn't exist".format(i))
+
 # Задача-2:
 # Напишите скрипт отображающий папки текущей директории
+
+
+def show_current_dir():
+    dir_list = os.listdir()
+    for f in dir_list:
+        if os.path.isdir(f):
+            print(f)
 
 # Задача-3:
 # Напишите скрипт создающий копию файла, из которого запущен данный скрипт
 
-# test git amend
+
+def copy_current_file():
+    split = str.split(__file__, ".")
+    shutil.copy(__file__, split[0] + "_copy." + split[1])
+
+
+# Additional functions for task hw05_normal.py
+
+def change_directory(new_path):
+    os.chdir(new_path)
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,7 @@
+import sys
+import lesson05.home_work.hw05_easy as my_utils
+
+
 # Задача-1:
 # Напишите небольшую консольную утилиту, позволяющую работать с папками текущей директории.
 # Утилита должна иметь меню выбора действия, в котором будут пункты:
@@ -10,3 +14,36 @@
 
 # Для решения данной задачи используйте алоритмы из задания easy,
 # оформленныйе в виде соответствующих функций, и импортированные в данный файл из easy.py
+
+
+# my_utils.copy_current_file()
+# print(sys.argv)
+
+
+def print_help():
+    print("cd - change directory --> example: 'python hw05_normal.py cd full_new_path'")
+    print("ls - list directory --> example: 'python hw05_normal.py ls'")
+    print("rmdir - remove directory --> example: 'python hw05_normal.py rmdir directory_name'")
+    print("mkdir - make directory --> example: 'python hw05_normal.py mkdir directory_name'")
+    print("exit - quit from script --> example: 'python hw05_normal.py exit'")
+
+
+action_container = {
+    "help": print_help,
+    "cd": my_utils.change_directory,
+    "ls": my_utils.show_current_dir,
+    "rmdir": my_utils.remove_folders,
+    "mkdir": my_utils.create_folders,
+    "exit": sys.exit
+}
+
+if len(sys.argv) > 1:
+    command = sys.argv[1]
+    command_args = sys.argv[2:]
+    try:
+        if len(command_args) > 0:
+            action_container[command](command_args[0])
+        else:
+            action_container[command]()
+    except KeyError as e:
+        print("Error: ", e)
