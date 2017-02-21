241a7bdeb29c1f442b3d7eb129ff6d79348d4a50
Denis Sorokin <denys-s@yit.co.il>
2017-02-21 14:09:02 +0200

    hw

--- lesson05/home_work/utils.py
+++ lesson05/home_work/utils.py
@@ -1,10 +1,19 @@
 import os
 
-
-def copy_file(current_file_name, copy_file_name):
-    path = os.getcwd()
-    print(path)
-    os.rename(path + "\\" + current_file_name, copy_file_name)
-
-
-copy_file("test.txt", "copy")
+# import shutil
+# import platform
+#
+#
+# def copy_file(*args):
+#     print(args)
+#     path_split = str.split(args[0][0], "/" if platform.system() is "Windows" else "\\")
+#     print(path_split)
+#     if bool(args[0][2]):
+#         dist_path = args[0][2]
+#     file_name_split = str.split(args[0][1], ".")
+#     shutil.copy(args[0][1], file_name_split[0] + "_copy." + file_name_split[1])
+#
+#
+#
+args = [1, 2, 3]
+print(bool(args[4]))
--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,3 +1,5 @@
+import math
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определть методы позволяющие вычислить: Площадь, высоту и периметр фигуры
 
@@ -5,3 +7,90 @@
 #  Предусмотреть в классе методы: проверка, является ли фигура равнобочной трапецией;
 #  вычисления: длины сторон, периметр, площадь.
 
+a = {"x": 1, "y": 1}
+b = {"x": 2, "y": 5}
+c = {"x": 5, "y": 1}
+
+aa = {"x": 1, "y": 1}
+bb = {"x": 2, "y": 3}
+cc = {"x": 5, "y": 3}
+dd = {"x": 6, "y": 1}
+
+
+class Figure:
+    def __init__(self, *args):
+        self.args = args
+
+
+class Triangle(Figure):
+    def __init__(self, a, b, c):
+        Figure.__init__(self, a, b, c)
+        self.A = a
+        self.B = b
+        self.C = c
+        self.a = self.basis(a, b)
+        self.b = self.basis(b, c)
+        self.c = self.basis(c, a)
+
+    @staticmethod
+    def height_a(a, b, c):
+        p = (a + b + c) / 2
+        return 2 * math.sqrt(p * (p - a) * (p - b) * (p - c)) / 2
+
+    @staticmethod
+    def basis(p01, p02):
+        return math.sqrt((p02["x"] - p01["x"]) ** 2 + (p02["y"] - p01["y"]) ** 2)
+
+    @property
+    def square(self):
+        return (self.a * Triangle.height_a(self.a, self.b, self.c)) / 2
+
+    @property
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+
+class Trapeze:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.ab = self.basis(a, b)
+        self.bc = self.basis(b, c)
+        self.cd = self.basis(c, d)
+        self.da = self.basis(d, a)
+
+    @staticmethod
+    def basis(p01, p02):
+        return math.sqrt((p02["x"] - p01["x"]) ** 2 + (p02["y"] - p01["y"]) ** 2)
+
+    @property
+    def is_right(self):
+        if self.ab == self.cd:
+            return self.bc, self.da, self.ab
+        elif self.bc == self.da:
+            return self.ab, self.cd, self.bc
+        else:
+            return False
+
+    @property
+    def perimeter(self):
+        return self.ab + self.bc + self.cd + self.da
+
+    @property
+    def square(self):
+        a = self.is_right[0]
+        b = self.is_right[1]
+        c = self.is_right[2]
+        return (a + b) / 4 * math.sqrt(4 * c ** 2 - (a - b) ** 2)
+
+
+# tr1 = Triangle(a, b, c)
+# print(tr1.square)
+# print(tr1.perimeter)
+#
+# trap1 = Trapeze(aa, bb, cc, dd)
+# print(trap1.is_right)
+# print(trap1.perimeter)
+# print(trap1.square)
